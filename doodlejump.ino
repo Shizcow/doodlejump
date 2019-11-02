3f5ce137c4e7e9cc3d170928ab543acb55224b90
Shizcow <pohl.devin@gmail.com>
2019-11-01 22:09:04 -0600

    Simple demo of platforms falling from the top of the screen

--- doodlejump.ino
+++ doodlejump.ino
@@ -30,19 +30,55 @@ MPU6050 mpu6050(Wire);
 
 MCUFRIEND_kbv tft(A3, A2, A1, A0, A4);  
 
+#define USEGYRO 0
+#define HEIGHT 320
+#define WIDTH 240
 
+class Vtft{
+ public:
+  Vtft(MCUFRIEND_kbv &tft_initilizer){
+    _tft = &tft_initilizer;
+    v_offset = 0;
+  }
+  void vertScroll(int16_t top, int16_t scrollines, int16_t offset){
+    v_offset=(v_offset+offset)%HEIGHT;
+    _tft->vertScroll(top, scrollines, v_offset);
+  }
+  void fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color){
+    _tft->fillRect(x, (y+v_offset)%HEIGHT, w, h, color);
+  }
+ private:
+  uint16_t v_offset;
+  MCUFRIEND_kbv* _tft;
+};
+
+Vtft vtft(tft);
+
+struct Platform{
+  int16_t x;
+  int16_t y;
+  int16_t w;
+};
+Platform platforms[10];
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   uint16_t ID = tft.readID();
   tft.begin(ID); 
   tft.fillScreen(BLACK);
+#if USEGYRO == 1
   Wire.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
+#endif
+
+  for(int i=0; i<10; ++i)
+    platforms[i].y = -1;
 }
 
+
 void loop() {
+#if USEGYRO == 1
   mpu6050.update();
   Serial.println("\nangleX: " );
   Serial.print(mpu6050.getAngleX());
@@ -50,8 +86,42 @@ void loop() {
   Serial.print(mpu6050.getAngleY());
   Serial.print("\tangleZ: " );
   Serial.print(mpu6050.getAngleZ());
+#endif
+
+  // presenting the platforms:
+  /*
+    Store an amount, maybe dynamic (could use data structures)
+    store as units, where y units are relative to the screen (can go into negatives -- these aren't rendered)
+    only store platforms so far below and above
+    when player moves, platforms are generated
+   */
+
+  if(rand()%100 < 5){
+    for(int i=0; i<10; ++i){
+      if(platforms[i].y == -1){
+	platforms[i].w = 10+rand()%20;
+	platforms[i].x = rand()%(WIDTH-platforms[i].w);
+	platforms[i].y = HEIGHT;
+	vtft.fillRect(platforms[i].x, platforms[i].y, platforms[i].w, 1, WHITE);
+	break;
+      }
+    }
+  }
   
-    Serial.println("========================================");
-    Serial.println("Calculating gyro offsets");
-    Serial.print("DO NOT MOVE MPU6050");
+  for(int i=0; i<10; ++i){
+    if(platforms[i].y != -1){
+      platforms[i].y--;
+      if(platforms[i].y == -1){
+	vtft.fillRect(platforms[i].x, platforms[i].y+1, platforms[i].w, 1, BLACK);
+      } 
+    }
+  }
+
+  
+  
+  //vtft.fillRect(0, 0, WIDTH, 1, RED);
+  vtft.vertScroll(0, HEIGHT, 1);
+  
+  delay(10);
+
 }
--- makefile
+++ makefile
@@ -3,4 +3,5 @@ BOARD_SUB = atmega2560
 ARDUINO_LIBS = Adafruit_GFX_Library MCUFRIEND_kbv  MPU6050_tockn Wire SPI
 MONITOR_PORT = /dev/ttyACM0
 ARDUINO_VERSION = 1811
+AVRDUDE_CONF = /etc/avrdude.conf
 include /usr/share/arduino/Arduino.mk
