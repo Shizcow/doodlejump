6fc3eb40a6c90fb045a3df346e298a9c8023b557
Shizcow <pohl.devin@gmail.com>
2019-11-01 19:11:33 -0600

    Successful interfacing with display and gyro

--- doodlejump.ino
+++ doodlejump.ino
@@ -1,136 +1,57 @@
-/*
+// accel libs
 
- TFT Pong
+#include <MPU6050_tockn.h>
+#include <Wire.h>
 
- This example for the Arduino screen reads the values
- of 2 potentiometers to move a rectangular platform
- on the x and y axes. The platform can intersect
- with a ball causing it to bounce.
+MPU6050 mpu6050(Wire);
 
- This example code is in the public domain.
+#include <Adafruit_GFX.h>
+#include <MCUFRIEND_kbv.h> 
 
- Created by Tom Igoe December 2012
- Modified 15 April 2013 by Scott Fitzgerald
+#define BLACK 0x0000
+#define NAVY 0x000F
+#define DARKGREEN 0x03E0
+#define DARKCYAN 0x03EF
+#define MAROON 0x7800
+#define PURPLE 0x780F
+#define OLIVE 0x7BE0
+#define LIGHTGREY 0xC618
+#define DARKGREY 0x7BEF
+#define BLUE 0x001F
+#define GREEN 0x07E0
+#define CYAN 0x07FF
+#define RED 0xF800
+#define MAGENTA 0xF81F
+#define YELLOW 0xFFE0
+#define WHITE 0xFFFF
+#define ORANGE 0xFD20
+#define GREENYELLOW 0xAFE5
+#define PINK 0xF81F
 
- http://www.arduino.cc/en/Tutorial/TFTPong
+MCUFRIEND_kbv tft(A3, A2, A1, A0, A4);  
 
- */
-
-#include <TFT.h>  // Arduino LCD library
-#include <SPI.h>
-
-// pin definition for the Uno
-#define cs   10
-#define dc   9
-#define rst  8
-
-// pin definition for the Leonardo
-// #define cs   7
-// #define dc   0
-// #define rst  1
-
-TFT TFTscreen = TFT(cs, dc, rst);
-
-// variables for the position of the ball and paddle
-int paddleX = 0;
-int paddleY = 0;
-int oldPaddleX, oldPaddleY;
-int ballDirectionX = 1;
-int ballDirectionY = 1;
-
-int ballSpeed = 10; // lower numbers are faster
-
-int ballX, ballY, oldBallX, oldBallY;
-
-
-// this function checks the position of the ball
-// to see if it intersects with the paddle
-bool inPaddle(int x, int y, int rectX, int rectY, int rectWidth, int rectHeight) {
-  bool result = false;
-
-  if ((x >= rectX && x <= (rectX + rectWidth)) &&
-      (y >= rectY && y <= (rectY + rectHeight))) {
-    result = true;
-  }
-
-  return result;
-}
-
-
-// this function determines the ball's position on screen
-void moveBall() {
-  // if the ball goes offscreen, reverse the direction:
-  if (ballX > TFTscreen.width() || ballX < 0) {
-    ballDirectionX = -ballDirectionX;
-  }
-
-  if (ballY > TFTscreen.height() || ballY < 0) {
-    ballDirectionY = -ballDirectionY;
-  }
-
-  // check if the ball and the paddle occupy the same space on screen
-  if (inPaddle(ballX, ballY, paddleX, paddleY, 20, 5)) {
-    ballDirectionX = -ballDirectionX;
-    ballDirectionY = -ballDirectionY;
-  }
-
-  // update the ball's position
-  ballX += ballDirectionX;
-  ballY += ballDirectionY;
-
-  // erase the ball's previous position
-  TFTscreen.fill(0, 0, 0);
-
-  if (oldBallX != ballX || oldBallY != ballY) {
-    TFTscreen.rect(oldBallX, oldBallY, 5, 5);
-  }
-
-
-  // draw the ball's current position
-  TFTscreen.fill(255, 255, 255);
-  TFTscreen.rect(ballX, ballY, 5, 5);
-
-  oldBallX = ballX;
-  oldBallY = ballY;
-
-}
 
 
 void setup() {
-  // initialize the display
-  TFTscreen.begin();
-  // black background
-  TFTscreen.background(0, 0, 0);
+  Serial.begin(9600);
+  uint16_t ID = tft.readID();
+  tft.begin(ID); 
+  tft.fillScreen(BLACK);
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
 }
 
 void loop() {
-
-  // save the width and height of the screen
-  int myWidth = TFTscreen.width();
-  int myHeight = TFTscreen.height();
-
-  // map the paddle's location to the position of the potentiometers
-  paddleX = map(analogRead(A0), 512, -512, 0, myWidth) - 20 / 2;
-  paddleY = map(analogRead(A1), 512, -512, 0, myHeight) - 5 / 2;
-
-  // set the fill color to black and erase the previous
-  // position of the paddle if different from present
-  TFTscreen.fill(0, 0, 0);
-
-  if (oldPaddleX != paddleX || oldPaddleY != paddleY) {
-    TFTscreen.rect(oldPaddleX, oldPaddleY, 20, 5);
-  }
-
-  // draw the paddle on screen, save the current position
-  // as the previous.
-  TFTscreen.fill(255, 255, 255);
-
-  TFTscreen.rect(paddleX, paddleY, 20, 5);
-  oldPaddleX = paddleX;
-  oldPaddleY = paddleY;
-
-  // update the ball's position and draw it on screen
-  if (millis() % ballSpeed < 2) {
-    moveBall();
-  }
+  mpu6050.update();
+  Serial.println("\nangleX: " );
+  Serial.print(mpu6050.getAngleX());
+  Serial.print("\tangleY: " );
+  Serial.print(mpu6050.getAngleY());
+  Serial.print("\tangleZ: " );
+  Serial.print(mpu6050.getAngleZ());
+  
+    Serial.println("========================================");
+    Serial.println("Calculating gyro offsets");
+    Serial.print("DO NOT MOVE MPU6050");
 }
--- makefile
+++ makefile
@@ -1,5 +1,6 @@
-BOARD_TAG = megaADK
-ARDUINO_LIBS = 
+BOARD_TAG = mega
+BOARD_SUB = atmega2560
+ARDUINO_LIBS = Adafruit_GFX_Library MCUFRIEND_kbv  MPU6050_tockn Wire SPI
 MONITOR_PORT = /dev/ttyACM0
 ARDUINO_VERSION = 1811
 include /usr/share/arduino/Arduino.mk
